05bbc4ae288378cfd5e50244bcc268016a2ea0a5
NedHuang <clch2cooch2ch3@gmail.com>
2021-04-18 00:33:26 +0800

    C++ note(class) not finished

---
+++ C++/play.cpp
@@ -0,0 +1,40 @@
+
+
+class BaseClass
+{
+public:
+    int PublicFunc(); // Declare a public member.
+protected:
+    int ProtectedFunc(); // Declare a protected member.
+private:
+    int PrivateFunc(); // Declare a private member.
+};
+
+// Declare two classes derived from BaseClass.
+class DerivedClass1 : public BaseClass
+{
+    void foo()
+    {
+        PublicFunc();
+        ProtectedFunc();
+        PrivateFunc(); // function is inaccessible
+    }
+};
+
+class DerivedClass2 : private BaseClass
+{
+    void foo()
+    {
+        PublicFunc();
+        ProtectedFunc();
+        PrivateFunc(); // function is inaccessible
+    }
+};
+
+int main()
+{
+    DerivedClass1 derived_class1;
+    DerivedClass2 derived_class2;
+    derived_class1.PublicFunc();
+    derived_class2.PublicFunc(); // function is inaccessible
+}
